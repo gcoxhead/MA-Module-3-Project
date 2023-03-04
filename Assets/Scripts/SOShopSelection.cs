4b82eab833c0ea0438e08f477f1d2296df615183
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2023-03-04 14:31:20 +0000

    update

---
+++ Assets/Scripts/SOShopSelection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Create Shop Item", menuName = "Create Shop Item")]
+public class SOShopSelection : ScriptableObject
+{
+
+    public Sprite icon;
+    public string iconName;
+    public string description;
+    public string cost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
